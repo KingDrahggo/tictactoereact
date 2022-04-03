533edf5796aed24d0eafc3f239d191fa42831c67
Gregory Reeves <gregreeves280@gmail.com>
2022-04-03 01:00:03 -0400

    working on getting clicks to work

--- src/GameBoard/GameBoard.js
+++ src/GameBoard/GameBoard.js
@@ -1,29 +1,73 @@
 import "./GameBoard.css";
+import React, {useState} from "react";
+
 
 const GameBoard = () => {
 // Start game
+//  const gameCellElements = document.querySelectorAll('[data-gameCell]');
+//  console.log(gameCellElements); 
 
+//  gameCellElements.forEach(gameCell=>{
+//      gameCell.addEventListener('click', handleClick, {once: true})
+//  })
+
+//  function handleClick(e){
+//      console.log('clicked')
+//  }
+
+    const xClass = "x";
+    const oClass = "o";
+    let oTurn = true;
+    console.log(oTurn)
+
+    if(oTurn === true){
+        oTurn = oClass
+        // oTurn = false;
+        console.log(oTurn)
+    } if (oTurn === false) {
+        oTurn = xClass
+        console.log(oTurn)
+    }
+    const [play, addClass] = useState(xClass);
+
+    const add = (e) => {
+        // e.preventDefault();
+        const cell = e.target
+    
+        console.log(cell)
+        
+    }
+   
+    // function placeMark(cell, currentClass){
+    //     cell.classList.add(currentClass)
+    // }
+
+    // function changeTurns(){
+    //     if(oTurn === true){
+    //        return oTurn === false
+    //     } else {
+    //         return oTurn === true
+    //     }
+    // }
 // Play game
 
 // Add Point to Player 1 or 2
 
 // Restart Game
 
-
-  
     return (
     
     <>
     <div className="gameBoard">
-      <div className="gameCell"></div>
-      <div className="gameCell"></div>
-      <div className="gameCell"></div>
-      <div className="gameCell"></div>
-      <div className="gameCell"></div>
-      <div className="gameCell"></div>
-      <div className="gameCell"></div>
-      <div className="gameCell"></div>
-      <div className="gameCell"></div>
+      <div onClick={add} className={`gameCell ${play}`} data-gamecell></div>
+      <div onClick={add} className={`gameCell ${play}`} data-gamecell></div>
+      <div onClick={add} className={`gameCell ${play}`} data-gamecell></div>
+      <div onClick={add} className={`gameCell ${play}`} data-gamecell></div>
+      <div onClick={add} className={`gameCell ${play}`} data-gamecell></div>
+      <div onClick={add} className={`gameCell ${play}`} data-gamecell></div>
+      <div onClick={add} className={`gameCell ${play}`} data-gamecell></div>
+      <div onClick={add} className={`gameCell ${play}`} data-gamecell></div>
+      <div onClick={add} className={`gameCell ${play}`} data-gamecell></div>
     </div>
     <div className="winner">
         <div className="winner-text">You Win!</div>

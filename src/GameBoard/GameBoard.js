f861a46f8097dbc125a8a8545f77f176a162a677
Gregory Reeves <gregreeves280@gmail.com>
2022-04-04 01:26:13 -0400

    its swapping between x and o now

--- src/GameBoard/GameBoard.js
+++ src/GameBoard/GameBoard.js
@@ -17,38 +17,26 @@ const GameBoard = () => {
 
     const xClass = "x";
     const oClass = "o";
-    let oTurn = true;
+    let oTurn ;
     console.log(oTurn)
 
-    if(oTurn === true){
-        oTurn = oClass
-        // oTurn = false;
-        console.log(oTurn)
-    } if (oTurn === false) {
-        oTurn = xClass
-        console.log(oTurn)
-    }
-    const [play, addClass] = useState(xClass);
+    const [play, addClass] = useState(oTurn);
 
     const add = (e) => {
-        // e.preventDefault();
-        const cell = e.target
-    
-        console.log(cell)
-        
+      const cell = e.target
+      const currentClass = oTurn ? oClass : xClass;
+      placeMark(cell,currentClass)
+      e.preventDefault()
+      switchTurns()
+    }
+
+    function placeMark(cell,currentClass){
+        addClass(currentClass)
     }
    
-    // function placeMark(cell, currentClass){
-    //     cell.classList.add(currentClass)
-    // }
-
-    // function changeTurns(){
-    //     if(oTurn === true){
-    //        return oTurn === false
-    //     } else {
-    //         return oTurn === true
-    //     }
-    // }
+    function switchTurns(){
+        oTurn =!oTurn
+    }
 // Play game
 
 // Add Point to Player 1 or 2

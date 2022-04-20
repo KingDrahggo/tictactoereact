6837cf8b9ba94277de514caf19e465405c24438a
Gregory Reeves <gregreeves280@gmail.com>
2022-04-19 22:21:15 -0400

    getting closer/ need to get oTurn to flip onclick

--- src/GameBoard/GameBoard.js
+++ src/GameBoard/GameBoard.js
@@ -1,5 +1,5 @@
 import "./GameBoard.css";
-import React, { useState, useRef} from "react";
+import React, { useState, useEffect } from "react";
 
 const GameBoard = () => {
   // Start game
@@ -16,15 +16,8 @@ const GameBoard = () => {
 
   const xClass = "x";
   const oClass = "o";
-  let oTurn = '';
-
-  // useEffect(() => {
-  //   switchTurns();
-  //   return () =>{ 
-  //   switchTurns();
-  //   console.log(oTurn);
-  // }
-  // },[dataGamecell1]);
+  let oTurn = "";
+  
 
   const [play1, addClass1] = useState(oTurn);
   const [play2, addClass2] = useState(oTurn);
@@ -35,34 +28,36 @@ const GameBoard = () => {
   const [play7, addClass7] = useState(oTurn);
   const [play8, addClass8] = useState(oTurn);
   const [play9, addClass9] = useState(oTurn);
-  
-  const boardCell1 = document.querySelectorAll(".gb1");
-  const boardCell2 = document.querySelectorAll(".gb2");
-  const boardCell3 = document.querySelectorAll(".gb3");
-  const boardCell4 = document.querySelectorAll(".gb4");
-  const boardCell5 = document.querySelectorAll(".gb5");
-  const boardCell6 = document.querySelectorAll(".gb6");
-  const boardCell7 = document.querySelectorAll(".gb7");
-  const boardCell8 = document.querySelectorAll(".gb8");
-  const boardCell9 = document.querySelectorAll(".gb9");
 
-  console.log(boardCell1);
-  const dataGamecell1 = useRef(boardCell1[0]);
-  const dataGamecell2 = useRef(boardCell2[0]);
-  const dataGamecell3 = useRef(boardCell3[0]);
-  const dataGamecell4 = useRef(boardCell4[0]);
-  const dataGamecell5 = useRef(boardCell5[0]);
-  const dataGamecell6 = useRef(boardCell6[0]);
-  const dataGamecell7 = useRef(boardCell7[0]);
-  const dataGamecell8 = useRef(boardCell8[0]);
-  const dataGamecell9 = useRef(boardCell9[0]);
-  console.log(dataGamecell1);
-  console.log(dataGamecell2);
-  console.log(dataGamecell3);
-  
+  // const boardCell1 = document.querySelectorAll(".gb1");
+  // const boardCell2 = document.querySelectorAll(".gb2");
+  // const boardCell3 = document.querySelectorAll(".gb3");
+  // const boardCell4 = document.querySelectorAll(".gb4");
+  // const boardCell5 = document.querySelectorAll(".gb5");
+  // const boardCell6 = document.querySelectorAll(".gb6");
+  // const boardCell7 = document.querySelectorAll(".gb7");
+  // const boardCell8 = document.querySelectorAll(".gb8");
+  // const boardCell9 = document.querySelectorAll(".gb9");
+
+  // console.log(boardCell1);
+  // const dataGamecell1 = useRef(boardCell1[0]);
+  // const dataGamecell2 = useRef(boardCell2[0]);
+  // const dataGamecell3 = useRef(boardCell3[0]);
+  // const dataGamecell4 = useRef(boardCell4[0]);
+  // const dataGamecell5 = useRef(boardCell5[0]);
+  // const dataGamecell6 = useRef(boardCell6[0]);
+  // const dataGamecell7 = useRef(boardCell7[0]);
+  // const dataGamecell8 = useRef(boardCell8[0]);
+  // const dataGamecell9 = useRef(boardCell9[0]);
+  // console.log(dataGamecell1);
+  // console.log(dataGamecell2);
+  // console.log(dataGamecell3);
   function switchTurns() {
-   oTurn= !oTurn
+   let newTurn = oTurn? xClass : oClass;
+   console.log(newTurn)
   }
+ 
+
 
   // const add1 = (e) => {
   //   console.log(e);
@@ -71,7 +66,7 @@ const GameBoard = () => {
   //   console.log(cell)
   //   switchTurns();
   //   addClass1(play1 === oTurn ? oClass : xClass);
-    
+
   //   console.log(oTurn)
   // };
 
@@ -82,62 +77,61 @@ const GameBoard = () => {
   //   console.log(cell)
   //   console.log(oTurn)
   //   addClass2(play2 === oTurn ? oClass : xClass);
-   
+
   //   console.log(oTurn)
   // };
-  
-  const add3 = (e) => {
-    console.log(e);
-    let cell = e.target.className;
-    console.log(oTurn)
-    addClass3(play3 === oTurn ? oClass : xClass);
-    console.log(oTurn)
-    // switchTurns();
-  };
-  const add4 = (e) => {
-    console.log(e);
-    let cell = e.target.className;
-    let currentClass = oTurn ? oClass : xClass;
-    addClass4(cell + currentClass);
-    switchTurns();
-  };
-  const add5 = (e) => {
-    console.log(e);
-    let cell = e.target.className;
-    let currentClass = oTurn ? oClass : xClass;
-    addClass5(cell + currentClass);
-    switchTurns();
-  };
-  const add6 = (e) => {
-    console.log(e);
-    let cell = e.target.className;
-    let currentClass = oTurn ? oClass : xClass;
-    addClass6(cell + currentClass);
-    switchTurns();
-  };
-  const add7 = (e) => {
-    console.log(e);
-    let cell = e.target.className;
-    let currentClass = oTurn ? oClass : xClass;
-    addClass7(cell + currentClass);
-    switchTurns();
-  };
-  const add8 = (e) => {
-    console.log(e);
-    let cell = e.target.className;
-    let currentClass = oTurn ? oClass : xClass;
-    addClass8(cell + currentClass);
-    switchTurns();
-  };
-  const add9 = (e) => {
-    console.log(e);
-    let cell = e.target.className;
-    console.log(cell)
-    let currentClass = oTurn ? oClass : xClass;
-    addClass9(cell + currentClass);
-    switchTurns();
-  };
-  
+
+  // const add3 = (e) => {
+  //   console.log(e);
+  //   let cell = e.target.className;
+  //   console.log(oTurn)
+  //   addClass3(play3 === oTurn ? oClass : xClass);
+  //   console.log(oTurn)
+  //   // switchTurns();
+  // };
+  // const add4 = (e) => {
+  //   console.log(e);
+  //   let cell = e.target.className;
+  //   let currentClass = oTurn ? oClass : xClass;
+  //   addClass4(cell + currentClass);
+  //   switchTurns();
+  // };
+  // const add5 = (e) => {
+  //   console.log(e);
+  //   let cell = e.target.className;
+  //   let currentClass = oTurn ? oClass : xClass;
+  //   addClass5(cell + currentClass);
+  //   switchTurns();
+  // };
+  // const add6 = (e) => {
+  //   console.log(e);
+  //   let cell = e.target.className;
+  //   let currentClass = oTurn ? oClass : xClass;
+  //   addClass6(cell + currentClass);
+  //   switchTurns();
+  // };
+  // const add7 = (e) => {
+  //   console.log(e);
+  //   let cell = e.target.className;
+  //   let currentClass = oTurn ? oClass : xClass;
+  //   addClass7(cell + currentClass);
+  //   switchTurns();
+  // };
+  // const add8 = (e) => {
+  //   console.log(e);
+  //   let cell = e.target.className;
+  //   let currentClass = oTurn ? oClass : xClass;
+  //   addClass8(cell + currentClass);
+  //   switchTurns();
+  // };
+  // const add9 = (e) => {
+  //   console.log(e);
+  //   let cell = e.target.className;
+  //   console.log(cell)
+  //   let currentClass = oTurn ? oClass : xClass;
+  //   addClass9(cell + currentClass);
+  //   switchTurns();
+  // };
 
   // function placeMark1(currentClass) {
   //   return addClass1(currentClass);
@@ -167,9 +161,6 @@ const GameBoard = () => {
   //   return addClass9(currentClass);
   // }
 
-
- 
-
   // Play game
 
   // Add Point to Player 1 or 2
@@ -179,83 +170,86 @@ const GameBoard = () => {
   return (
     <>
       <div className="gameBoard">
-        <div className="gb1">
-          <div
-            ref={dataGamecell1}
-            onClick={() =>{
-              switchTurns()
-              addClass1(play1 === oTurn? oClass : xClass)
-            }}
-            className={`gameCell ${play1}`}
-            data-gamecell
-          ></div>
-        </div>
-        <div className="gb2">
-          <div
-            ref={dataGamecell2}
-            onClick={() =>{
-              switchTurns()
-              addClass2(play2 === oTurn? oClass : xClass)
-            }}
-            className={`gameCell ${play2}`}
-            data-gamecell
-          ></div>
-        </div>
-        <div className="gb3">
-          <div
-            ref={dataGamecell3}
-            onClick={add3}
-            className={`gameCell ${play3}`}
-            data-gamecell
-          ></div>
-        </div>
-        <div className="gb4">
-          <div
-            ref={dataGamecell4}
-            onClick={add4}
-            className={`gameCell ${play4}`}
-            data-gamecell
-          ></div>
-        </div>
-        <div className="gb5">
-          <div
-            ref={dataGamecell5}
-            onClick={add5}
-            className={`gameCell ${play5}`}
-            data-gamecell
-          ></div>
-        </div>
-        <div className="gb6">
-          <div
-            ref={dataGamecell6}
-            onClick={add6}
-            className={`gameCell ${play6}`}
-            data-gamecell
-          ></div>
-        </div>
-        <div className="gb7">
-          <div
-            ref={dataGamecell7}
-            onClick={add7}
-            className={`gameCell ${play7}`}
-            data-gamecell
-          ></div>
-        </div>
-        <div className="gb8">
-          <div
-            ref={dataGamecell8}
-            onClick={add8}
-            className={`gameCell ${play8}`}
-            data-gamecell
-          ></div>
-        </div>
-        <div className="gb9">
-          <div
-            ref={dataGamecell9}
-            onClick={add9}
-            className={`gameCell ${play9}`}
-          ></div>
-        </div>
+        <div
+          onClick={() => {
+          
+            addClass1(oTurn ? oClass : xClass);
+            switchTurns();
+          }}
+          className={`gameCell ${play1}`}
+          data-gamecell
+        ></div>
+
+        <div
+          onClick={() => {
+            switchTurns();
+            addClass2(oTurn ? oClass : xClass);
+          }}
+          className={`gameCell ${play2}`}
+          data-gamecell
+        ></div>
+
+        <div
+          onClick={() => {
+            switchTurns();
+            addClass3(play3 === oTurn ? oClass : xClass);
+          }}
+          className={`gameCell ${play3}`}
+          data-gamecell
+        ></div>
+
+        <div
+          onClick={() => {
+            switchTurns();
+            addClass4(play4 === oTurn ? oClass : xClass);
+          }}
+          className={`gameCell ${play4}`}
+          data-gamecell
+        ></div>
+
+        <div
+          onClick={() => {
+            switchTurns();
+            addClass5(play5 === oTurn ? oClass : xClass);
+          }}
+          className={`gameCell ${play5}`}
+          data-gamecell
+        ></div>
+
+        <div
+          onClick={() => {
+            switchTurns();
+            addClass6(play6 === oTurn ? oClass : xClass);
+          }}
+          className={`gameCell ${play6}`}
+          data-gamecell
+        ></div>
+
+        <div
+          onClick={() => {
+            switchTurns();
+            addClass7(play7 === oTurn ? oClass : xClass);
+          }}
+          className={`gameCell ${play7}`}
+          data-gamecell
+        ></div>
+
+        <div
+          onClick={() => {
+            switchTurns();
+            addClass8(play8 === oTurn ? oClass : xClass);
+          }}
+          className={`gameCell ${play8}`}
+          data-gamecell
+        ></div>
+
+        <div
+          onClick={() => {
+            switchTurns();
+            addClass9(play9 === oTurn ? oClass : xClass);
+          }}
+          className={`gameCell ${play9}`}
+        ></div>
       </div>
       <div className="winner">
         <div className="winner-text">You Win!</div>
